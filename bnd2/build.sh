dee10d1ff0194ccb53faacfe7919eafbccd8d2e2
Liang Ding <d@b3log.org>
2019-07-16 16:47:50 +0800

    :art: Mac 构建脚本

---
+++ bnd2/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export GOPROXY=https://goproxy.io
+echo Building Kernel
+export GOOS=windows
+export GOARCH=amd64
+go build -v -o electron/bnd2.exe -ldflags "-s -w -H=windowsgui"
+
+export GOOS=darwin
+export GOARCH=amd64
+go build -v -o electron/bnd2 -ldflags "-s -w"
+
+echo Building UI
+cd electron
+npm install && npm run dist
+cd ..
